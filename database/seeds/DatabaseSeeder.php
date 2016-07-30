934556f88886de4b1d86b9cc96249f81fe0c0d08
Sonu <sonuyadav919@gmail.com>
2016-07-30 22:17:39 +0530

    Database seeder updated for country state & city table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(CountryTableSeeder::class);
+        $this->call(StateTableSeeder::class);
+        $this->call(CityTableSeeder::class);
     }
 }
