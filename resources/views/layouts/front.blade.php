0d4ec352a15c1d1b4d2f9f67909039d8587ca744
Sonu <sonuyadav919@gmail.com>
2016-08-20 21:35:59 +0530

    Front end css updated

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -29,6 +29,8 @@
     <!-- Theme CSS -->
     <link href="{{asset('front/css/creative.min.css')}}" rel="stylesheet">
 
+    <link href="{{asset('front/css/custom.css')}}" rel="stylesheet">
+
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
